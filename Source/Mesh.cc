436bd2f1066310c3cb1d74ba881d29bf6e781cb0
T.J. Corona <tjcorona@gmail.com>
2016-02-21 17:29:31 -0500

    Modifications to test executable to keep things up and running.

--- Source/Mesh.cc
+++ Source/Mesh.cc
@@ -33,10 +33,10 @@ int main(int /*argc*/,char** /*argv*/)
   unsigned nPerimeterEdge = 1;
 
   DTDelaunayMesh mesh;
-  mesh.AddPerimeterPoint(p0);
-  mesh.AddPerimeterPoint(p1);
-  mesh.AddPerimeterPoint(p2);
-  mesh.AddPerimeterPoint(p3);
+  // mesh.AddPerimeterPoint(p0);
+  // mesh.AddPerimeterPoint(p1);
+  // mesh.AddPerimeterPoint(p2);
+  // mesh.AddPerimeterPoint(p3);
 
 /*
   mesh.AddPerimeterPoint(p0);
@@ -57,10 +57,10 @@ int main(int /*argc*/,char** /*argv*/)
 
   for (unsigned i=0;i<nPoly;i++)
   {
-    mesh.AddInteriorPoint(DTPoint(5.+4.9*cos(2.*M_PI*i/nPoly),
-    				  5.+4.9*sin(2.*M_PI*i/nPoly)));
-    // mesh.AddPerimeterPoint(DTPoint(5.+4.9*cos(2.*M_PI*i/nPoly),
-    // 				   5.+4.9*sin(2.*M_PI*i/nPoly)));
+    // mesh.AddInteriorPoint(DTPoint(5.+4.9*cos(2.*M_PI*i/nPoly),
+    // 				  5.+4.9*sin(2.*M_PI*i/nPoly)));
+    mesh.AddPerimeterPoint(DTPoint(5.+4.9*cos(2.*M_PI*i/nPoly),
+    				   5.+4.9*sin(2.*M_PI*i/nPoly)));
 
     std::set<const DTTriangle*> illegalTriangles;
     if (!mesh.TestDelaunayCondition(illegalTriangles))
@@ -68,23 +68,43 @@ int main(int /*argc*/,char** /*argv*/)
     DrawMesh(mesh,illegalTriangles,canvas);
   }
 
-  for (unsigned i=0;i<nPoly;i++)
-  {
-    mesh.AddInteriorPoint(DTPoint(5.+2.5*cos(2.*M_PI*i/nPoly + M_PI),
-  				  5.+2.5*sin(2.*M_PI*i/nPoly + M_PI)));
+  // for (unsigned i=0;i<nPoly;i++)
+  // {
+  //   mesh.AddInteriorPoint(DTPoint(5.+2.5*cos(2.*M_PI*i/nPoly + M_PI),
+  // 				  5.+2.5*sin(2.*M_PI*i/nPoly + M_PI)));
 
-    std::set<const DTTriangle*> illegalTriangles;
-    if (!mesh.TestDelaunayCondition(illegalTriangles))
-      std::cout<<"Failed"<<std::endl;
-    DrawMesh(mesh,illegalTriangles,canvas);
-  }
+  //   std::set<const DTTriangle*> illegalTriangles;
+  //   if (!mesh.TestDelaunayCondition(illegalTriangles))
+  //     std::cout<<"Failed"<<std::endl;
+  //   DrawMesh(mesh,illegalTriangles,canvas);
+  // }
 
   // Random::GetInstance().Seed(0);
 
-  for (unsigned i=0;i<100;i++)
+  // for (unsigned i=0;i<500;i++)
+  // {
+  //   DTPoint p(x_min + Random::GetInstance().Uniform(1000)/1000.*(x_max-x_min),
+  // 	      y_min + Random::GetInstance().Uniform(1000)/1000.*(y_max-y_min));
+
+  //   mesh.AddInteriorPoint(p);
+
+  //   std::set<const DTTriangle*> illegalTriangles;
+  //   if (!mesh.TestDelaunayCondition(illegalTriangles))
+  //     std::cout<<"Failed"<<std::endl;
+  //   DrawMesh(mesh,illegalTriangles,canvas);
+  // }
+
+  for (unsigned i=0;i<1000;i++)
   {
-    DTPoint p(x_min + Random::GetInstance().Uniform(1000)/1000.*(x_max-x_min),
-  	      y_min + Random::GetInstance().Uniform(1000)/1000.*(y_max-y_min));
+    // uniform distribution
+    double r = 4.9*pow(Random::GetInstance().Uniform(1000)/1000.,.5);
+    // center weighted distribution
+    // double r = 4.9*pow(Random::GetInstance().Uniform(1000)/1000.,2.);
+    // edge weighted distribution
+    // double r = 4.9*pow(Random::GetInstance().Uniform(1000)/1000.,.25);
+    double theta = 2.*M_PI*Random::GetInstance().Uniform(1000)/1000.;
+
+    DTPoint p(5. + r*cos(theta), 5. + r*sin(theta));
 
     mesh.AddInteriorPoint(p);
 
@@ -94,33 +114,33 @@ int main(int /*argc*/,char** /*argv*/)
     DrawMesh(mesh,illegalTriangles,canvas);
   }
 
-  std::vector<DTPoint> points;
-  std::vector<int> pointIndex;
-  unsigned counter = 0;
+  // std::vector<DTPoint> points;
+  // std::vector<int> pointIndex;
+  // unsigned counter = 0;
 
-  unsigned nGridPoints = 10;
+  // unsigned nGridPoints = 10;
 
-  for (unsigned i=0;i<nGridPoints;i++)
-  {
-    for (unsigned j=0;j<nGridPoints;j++)
-    {
-      points.push_back(DTPoint(x_min + ((i+.5)/nGridPoints)*(x_max-x_min),
-  			       y_min + ((j+.5)/nGridPoints)*(y_max-y_min)));
+  // for (unsigned i=0;i<nGridPoints;i++)
+  // {
+  //   for (unsigned j=0;j<nGridPoints;j++)
+  //   {
+  //     points.push_back(DTPoint(x_min + ((i+.5)/nGridPoints)*(x_max-x_min),
+  // 			       y_min + ((j+.5)/nGridPoints)*(y_max-y_min)));
 
-      pointIndex.push_back(counter++);
-    }
-  }
+  //     pointIndex.push_back(counter++);
+  //   }
+  // }
 
-  Random::GetInstance().Shuffle(pointIndex);
+  // Random::GetInstance().Shuffle(pointIndex);
 
-  for (std::vector<int>::iterator it=pointIndex.begin();it!=pointIndex.end();++it)
-  {
-    mesh.AddInteriorPoint(points[*it]);
-    std::set<const DTTriangle*> illegalTriangles;
-    if (!mesh.TestDelaunayCondition(illegalTriangles))
-      std::cout<<"Failed"<<std::endl;
-    DrawMesh(mesh,illegalTriangles,canvas);
-  }
+  // for (std::vector<int>::iterator it=pointIndex.begin();it!=pointIndex.end();++it)
+  // {
+  //   mesh.AddInteriorPoint(points[*it]);
+  //   std::set<const DTTriangle*> illegalTriangles;
+  //   if (!mesh.TestDelaunayCondition(illegalTriangles))
+  //     std::cout<<"Failed"<<std::endl;
+  //   DrawMesh(mesh,illegalTriangles,canvas);
+  // }
 
   return 0;
 }
