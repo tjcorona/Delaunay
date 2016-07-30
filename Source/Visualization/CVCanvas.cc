e60717a21abdcdedc8d8e1c1393c9a5520879d65
T.J. Corona <tjcorona@gmail.com>
2016-07-30 09:21:34 -0400

    Add transparency, line/fill coloring.

--- Source/Visualization/CVCanvas.cc
+++ Source/Visualization/CVCanvas.cc
@@ -38,6 +38,7 @@ CVCanvas::CVCanvas(double dim_x_min,
 
   image = cv::Mat::zeros(dim[1],dim[0],CV_8UC3);
   image = cv::Scalar(255,255,255) - image;
+  cloneImage = image.clone();
 
   pointRadius = 4;
 
@@ -47,6 +48,12 @@ CVCanvas::CVCanvas(double dim_x_min,
 void CVCanvas::Draw(const Function& function, double* domain,
 		    const Color& color)
 {
+  if (!color.alpha)
+    return;
+
+  SetAlpha(color.alpha);
+  cv::Mat* im = PreDraw();
+
   double dmn[2] = {(domain ? domain[0] : dim_x[0]),
                    (domain ? domain[1] : dim_x[1])};
 
@@ -75,7 +82,7 @@ void CVCanvas::Draw(const Function& function, double* domain,
       {
         const cv::Point *pts = (const cv::Point*) cv::Mat(cvPointVec).data;
         int npts = cv::Mat(cvPointVec).rows;
-        cv::polylines(image,&pts, &npts, 1, false, ColorToCVScalar(color), 1,8);
+        cv::polylines(*im,&pts, &npts, 1, false, ColorToCVScalar(color), 1,8);
         cvPointVec.clear();
       }
     }
@@ -98,50 +105,153 @@ void CVCanvas::Draw(const Function& function, double* domain,
   {
     const cv::Point *pts = (const cv::Point*) cv::Mat(cvPointVec).data;
     int npts = cv::Mat(cvPointVec).rows;
-    cv::polylines(image,&pts, &npts, 1, false, ColorToCVScalar(color), 1, 8);
+    cv::polylines(*im,&pts, &npts, 1, false, ColorToCVScalar(color), 1, 8);
   }
+
+  PostDraw();
 }
 
 void CVCanvas::Draw(const Point& point,const Color& color)
 {
-  cv::circle(image,PointToCVPoint(point),pointRadius,ColorToCVScalar(color),-1,8);
+  SetAlpha(color.alpha);
+  cv::Mat* im = PreDraw();
+
+  cv::circle(*im,PointToCVPoint(point),pointRadius,ColorToCVScalar(color),-1,8);
+
+  PostDraw();
 }
 
-void CVCanvas::Draw(const Triangle& triangle,const Color& color)
+void CVCanvas::Draw(const Triangle& triangle,const Color& lineColor,
+		    const Color& fillColor)
 {
-  cv::line(image,PointToCVPoint(triangle.A),PointToCVPoint(triangle.B),ColorToCVScalar(color),1,8);
-  cv::line(image,PointToCVPoint(triangle.B),PointToCVPoint(triangle.C),ColorToCVScalar(color),1,8);
-  cv::line(image,PointToCVPoint(triangle.C),PointToCVPoint(triangle.A),ColorToCVScalar(color),1,8);
+  if (!lineColor.alpha && !fillColor.alpha)
+    return;
+
+  const cv::Point points[3] = {PointToCVPoint(triangle.A),
+			       PointToCVPoint(triangle.B),
+			       PointToCVPoint(triangle.C)};
+  cv::Mat* im;
+
+  if (fillColor.alpha)
+  {
+    SetAlpha(fillColor.alpha);
+    im = PreDraw();
+
+    fillConvexPoly(*im,points,3,ColorToCVScalar(fillColor),8);
+
+    if (lineColor.alpha != fillColor.alpha)
+      PostDraw();
+  }
+
+  if (lineColor.alpha != fillColor.alpha)
+  {
+    SetAlpha(lineColor.alpha);
+    im = PreDraw();
+  }
+
+  if (lineColor.alpha)
+  {
+    cv::line(*im,points[0],points[1],ColorToCVScalar(lineColor),1,8);
+    cv::line(*im,points[1],points[2],ColorToCVScalar(lineColor),1,8);
+    cv::line(*im,points[2],points[0],ColorToCVScalar(lineColor),1,8);
+  }
+
+  PostDraw();
 }
 
-void CVCanvas::Draw(const Polygon& polygon,const Color& color)
+void CVCanvas::Draw(const Polygon& polygon,const Color& lineColor,
+		    const Color& fillColor)
 {
+  if (!lineColor.alpha && !fillColor.alpha)
+    return;
+
+  std::vector<cv::Point> points;
   const unsigned size = polygon.Vertices.size();
   for (unsigned i=0;i<size;i++)
   {
-    cv::line(image,PointToCVPoint(polygon.Vertices[i]),
-  	     PointToCVPoint(polygon.Vertices[(i+1)%size]),
-  	     ColorToCVScalar(color),1,8);
+    points.push_back(PointToCVPoint(polygon.Vertices[i]));
   }
+
+  cv::Mat* im;
+
+  if (fillColor.alpha)
+  {
+    SetAlpha(fillColor.alpha);
+    im = PreDraw();
+
+    const cv::Point* p = &points[0];
+    const int sz = static_cast<int>(size);
+
+    fillPoly(*im,&p,&sz,1,ColorToCVScalar(fillColor),0);
+
+    if (lineColor.alpha != fillColor.alpha)
+      PostDraw();
+  }
+
+  if (lineColor.alpha != fillColor.alpha)
+  {
+    SetAlpha(lineColor.alpha);
+    im = PreDraw();
+  }
+
+  for (unsigned i=0;i<size;i++)
+  {
+    cv::line(*im,points[i],points[(i+1)%size],ColorToCVScalar(lineColor),1,8);
+  }
+
+  PostDraw();
 }
 
-void CVCanvas::Draw(const Circle& circle,const Color& color)
+void CVCanvas::Draw(const Circle& circle,const Color& lineColor,
+		    const Color& fillColor)
 {
-  cv::circle(image,PointToCVPoint(circle.Center()),
-	     circle.Radius()*conversionRatio,ColorToCVScalar(color),1,8);
-}
+  if (!lineColor.alpha && !fillColor.alpha)
+    return;
 
-void CVCanvas::Update()
-{
-  cv::imshow("Image",image);
-  cv::waitKey(delay_ms);
-  image = cv::Mat::zeros(dim[0],dim[1],CV_8UC3);
-  image = cv::Scalar(255,255,255) - image;
-  cv::imshow("Image",image);
+  cv::Mat* im;
+
+  if (lineColor == fillColor)
+  {
+    SetAlpha(fillColor.alpha);
+    im = PreDraw();
+    cv::circle(*im,PointToCVPoint(circle.Center()),
+	       circle.Radius()*conversionRatio,ColorToCVScalar(lineColor),-1,8);
+    PostDraw();
+    return;
+  }
+
+  if (fillColor.alpha)
+  {
+    SetAlpha(fillColor.alpha);
+    im = PreDraw();
+    cv::circle(*im,PointToCVPoint(circle.Center()),
+	       circle.Radius()*conversionRatio,ColorToCVScalar(fillColor),-1,8);
+
+    if (lineColor.alpha != fillColor.alpha)
+      PostDraw();
+  }
+
+  if (lineColor.alpha)
+  {
+    if (lineColor.alpha != fillColor.alpha)
+    {
+      SetAlpha(lineColor.alpha);
+      im = PreDraw();
+    }
+    cv::circle(*im,PointToCVPoint(circle.Center()),
+	       circle.Radius()*conversionRatio,ColorToCVScalar(lineColor),1,8);
+    PostDraw();
+  }
 }
 
 void CVCanvas::Draw(const ParametricCurve& curve,const Color& color)
 {
+  if (!color.alpha)
+    return;
+
+  SetAlpha(color.alpha);
+  cv::Mat* im = PreDraw();
+
   double min_step = (dim_x[1] - dim_x[0])/dim[0];
 
   double t = 0;
@@ -170,19 +280,53 @@ void CVCanvas::Draw(const ParametricCurve& curve,const Color& color)
     cvPointVec.push_back(PointToCVPoint(pointVec.at(i)));
   const cv::Point *pts = (const cv::Point*) cv::Mat(cvPointVec).data;
   int npts = cv::Mat(cvPointVec).rows;
-  cv::polylines(image,&pts, &npts, 1, false, ColorToCVScalar(color), 1, 8);
+  cv::polylines(*im,&pts, &npts, 1, false, ColorToCVScalar(color), 1, 8);
+
+  PostDraw();
 }
 
 
 cv::Scalar CVCanvas::ColorToCVScalar(const Color& color)
 {
-  return cv::Scalar(color.blue,color.green,color.red);
+  return cv::Scalar(color.blue,color.green,color.red,color.alpha);
 }
 
 cv::Point CVCanvas::PointToCVPoint(const Point& point)
 {
   return cv::Point((point.x - dim_x[0])*conversionRatio,
         	   (dim_y[1] - point.y)*conversionRatio);
+}
+
+void CVCanvas::Update()
+{
+  cv::imshow("Image",image);
+  cv::waitKey(delay_ms);
+  image = cv::Mat::zeros(dim[0],dim[1],CV_8UC3);
+  image = cv::Scalar(255,255,255) - image;
+  cv::imshow("Image",image);
+}
+
+cv::Mat* CVCanvas::PreDraw()
+{
+  if (alpha == 255)
+  {
+    return &image;
+  }
+  else
+  {
+    cloneImage = image.clone();
+    return &cloneImage;
+  }
+}
+
+void CVCanvas::PostDraw()
+{
+  if (alpha != 255)
+  {
+    double normalizedAlpha = (alpha/255.);
+    cv::addWeighted(cloneImage, normalizedAlpha,
+    		    image, 1. - normalizedAlpha, 0, image);
+  }
 }
 
 }
